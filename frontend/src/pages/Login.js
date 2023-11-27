1c82ad7d74b5a5c6f12d541aee94dd027eba4702
Jiaming Sun <mr_internet1969@outlook.com>
2023-11-27 20:20:25 +0000

    fine tuned login page css

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,31 +10,31 @@ export const Login = () => {
         backgroundImage: `url(${bg})`,
       }}
     >
-      <div class="relative font-bold border-[#48ff91] text-[#48f6ff] text-center flex flex-col gap-4 mx-auto border-4 rounded-md px-4 w-[30%] py-6 shadow-3xl shadow-[#48ff91]">
+      <div class="relative max-w-[500px] w-[30%] font-bold border-[#48ff91] text-[#48f6ff] text-center flex flex-col gap-4 mx-auto border-4 rounded-lg px-4 py-10 shadow-3xl shadow-[#48ff91]">
         <h1 class="text-4xl uppercase font-semibold">Login</h1>
-        <div class="flex flex-col gap-2 w-[70%] mx-auto text-[#48f6ff]">
+        <div class="flex flex-col gap-2 pt-5 w-[70%] mx-auto text-[#48f6ff]">
           <label class="text-xl font-semibold text-left" for="">
             Username
           </label>
           <input
-            class="outline-none px-4 py-2 rounded-full bg-transparent border-2 text-yellow-300"
+            class="outline-none px-4 py-2 rounded-full bg-transparent border-2 outline-[#48f6ff] placeholder-yellow-100 "
             type="text"
             placeholder="Username"
           />
         </div>
-        <div class="flex flex-col gap-2 w-[70%] mx-auto text-[#48f6ff]">
+        <div class="flex flex-col gap-2 py-4 w-[70%] mx-auto text-[#48f6ff]">
           <label class="text-xl font-semibold text-left" for="">
             Password
           </label>
           <input
-            class="outline-none px-4 py-2 rounded-full bg-transparent border-2  text-yellow-300"
+            class="outline-none px-4 py-2 rounded-full bg-transparent border-2 outline-[#48f6ff] placeholder-yellow-100"
             type="text"
             placeholder="Password"
           />
         </div>
-        <div class="text-md flex gap-4 mx-auto items-center">
+        <div class="text-md flex gap-4 pb-4 mx-auto items-center">
           <input
-            class="text-xl font-semibold text-left"
+            class="text-xl font-semibold text-left checked:bg-[#48ff91]"
             type="checkbox"
             name="Remember me"
             id=""
@@ -47,7 +47,7 @@ export const Login = () => {
             Forgot Password?
           </Link>
         </div>
-        <button class="bg-purple-700 px-4 py-2 rounded-md text-xl w-[200px] mx-auto hover:bg-transparent hover:border-purple-600 border-2 border-purple-500">
+        <button class="bg-purple-700 px-4 py-3 rounded-md text-xl w-[200px] mx-auto hover:bg-transparent hover:border-purple-600 border-2 border-purple-500">
           Login
         </button>
       </div>
