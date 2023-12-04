1460a559a3f06aa0cb3f34019e4e26804d689400
Jiaming Sun <mr_internet1969@outlook.com>
2023-12-04 12:28:49 +0000

    modified hardhad config - solidity version

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("@nomicfoundation/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "^0.8.20",
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
