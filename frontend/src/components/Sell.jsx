c6f04318af0c24a877f0d5214c084f7230d2883f
Jiaming Sun <mr_internet1969@outlook.com>
2023-12-09 00:57:47 +0000

    finalizing validation and error handling for sell

--- frontend/src/components/Sell.jsx
+++ frontend/src/components/Sell.jsx
@@ -12,46 +12,82 @@ export default function Sell() {
   const location = useLocation();
   const { img_src, name, description, token_id, nft_address } = location.state;
   const [loadingController, setloadingController] = useState(false);
+  const [buttonLoading, setbuttonLoading] = useState(false);
   const [timeSetterbox, setTimeSetterbox] = useState(true);
   const [price, setPrice] = useState(null);
   const [timeSetterboxStyle, setTimeSetterboxStyle] = useState("bg-gray-500");
+  const [message, setMessage] = useState("");
+  const [messageClass, setMessageClass] = useState("");
+
+  const formValid = async () => {
+    if (!price) {
+      setMessage("Price cannot be empty");
+      setMessageClass("font-bold text-xl text-red-600");
+      return false;
+    }
+
+    if (Number(price) <= 0) {
+      setMessage("Price cannot be negative");
+      setMessageClass("font-bold text-xl text-red-600");
+      return false;
+    }
+
+    setMessage("");
+    setMessageClass("");
+    return true;
+  };
 
   const buttonHandler = async (e) => {
     e.preventDefault();
-    setloadingController(true);
 
-    //get the nft contract
-    const nftContract = await getContract();
+    setbuttonLoading(true);
+    if (await formValid()) {
+      setloadingController(true);
+      try {
+        //get the nft contract
+        const nftContract = await getContract();
 
-    // get the market place contract
-    const marketPlace = await getMarketContract();
+        // get the market place contract
+        const marketPlace = await getMarketContract();
 
-    // sell add the nft to the market place
-    const address = window.localStorage.getItem("currentAddr");
+        // sell add the nft to the market place
+        const address = window.localStorage.getItem("currentAddr");
 
-    await nftContract.methods
-      .approve(marketPlace.options.address, token_id)
-      .send({ from: address });
+        await nftContract.methods
+          .approve(marketPlace.options.address, token_id)
+          .send({ from: address });
 
-    const weiprice = Number(Web3.utils.toWei(price, "ether"));
+        const weiprice = Number(Web3.utils.toWei(price, "ether"));
 
-    await marketPlace.methods
-      .sellNft721(nftContract.options.address, Number(token_id), weiprice)
-      .send({ from: address });
+        await marketPlace.methods
+          .sellNft721(nftContract.options.address, Number(token_id), weiprice)
+          .send({ from: address });
 
-    const puttingMarketplaceBody = {
-      token_id: token_id,
-      nft_address: nftContract.options.address,
-      owner: address,
-      price: Number(price),
-    };
-    console.log(puttingMarketplaceBody);
-    const response = await axios.put(
-      "http://localhost:4988/putNftInMarketplace",
-      puttingMarketplaceBody
-    );
+        const puttingMarketplaceBody = {
+          token_id: token_id,
+          nft_address: nftContract.options.address,
+          owner: address,
+          price: Number(price),
+        };
+        console.log(puttingMarketplaceBody);
+        const response = await axios.put(
+          "http://localhost:4988/putNftInMarketplace",
+          puttingMarketplaceBody
+        );
 
-    setloadingController(false);
+        setMessage("Submission successful!");
+        setMessageClass("font-bold text-xl text-[#48f9ff]");
+        setloadingController(false);
+      } catch (error) {
+        console.error(error);
+        setMessage("Submission failed!");
+        setMessageClass("font-bold text-lg text-red-600");
+        setloadingController(false);
+      }
+    }
+    setTimeout(() => {
+      setbuttonLoading(false);
+    }, 500);
   };
 
   const auctionTick = () => {
@@ -153,18 +189,49 @@ export default function Sell() {
               >
                 Set Your Price:
               </label>
-              <input
-                class="p-2 rounded-xl border mb-5"
-                type="number"
-                name="Price"
-                placeholder="Enter price you want to sell for"
-                onChange={(e) => setPrice(e.target.value)}
-              />
+              <div class="flex justify-between items-center">
+                <input
+                  class="p-2 rounded-xl border mb-3 pl-4 text-xl w-[60%]"
+                  type="number"
+                  name="Price"
+                  placeholder="Enter price"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+                <div class="font-bold text-3xl text-white pr-10 mb-4">ETH</div>
+              </div>
+              <p className={messageClass}>{message}</p>
               <button
+                type="submit"
+                className="bg-slate-800 flex justify-center items-center w-full rounded-xl text-3xl font-bold text-white px-4 py-2 hover:scale-105 duration-300"
                 onClick={buttonHandler}
-                class="bg-slate-800 rounded-xl text-3xl font-bold text-white py-2 mb-5 hover:scale-105 duration-300"
               >
-                Sell !
+                {buttonLoading ? (
+                  <>
+                    <svg
+                      class="mr-5 h-6 w-6 animate-spin text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    <span> Processing... </span>
+                  </>
+                ) : (
+                  "Sell !"
+                )}
               </button>
             </form>
           </div>
