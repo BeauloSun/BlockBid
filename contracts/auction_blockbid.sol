24b64d3bb7a35aeab2ef84cbd2fe13b960669fc3
Jiaming Sun <mr_internet1969@outlook.com>
2023-12-17 16:10:45 +0000

    auction contract basic functionality complete

--- contracts/auction_blockbid.sol
+++ contracts/auction_blockbid.sol
@@ -12,6 +12,7 @@ error TheTokenDoesNotBelongToTheSender();
 error DoNotHaveApprovalToSellNft();
 error AuctionNotEnded();
 error BidNotHighest();
+error AuctionItemNotExists(uint256 _tokenId);
 
 contract BlockBid is ReentrancyGuard{
 
@@ -19,14 +20,20 @@ contract BlockBid is ReentrancyGuard{
         uint256 tokenId;
         address payable owner;
         uint256 price;
+    }
+
+    struct auctionListing721{
+        uint256 tokenId;
+        address payable owner;
+        uint256 minPrice;
         uint256 auctionEndTime;
         address highestBidder;
         uint256 highestBid;
         bool ended;
-        uint256 buyoutPrice;
     }
 
-    mapping (address => mapping(uint256 => listing721)) nftListed721;
+    mapping(uint256 => listing721) nftListed721;
+    mapping (uint256 => auctionListing721) auctionNftListed721;
     uint256[] ListedTokens721;
     
     modifier Owner721(address nftAddress ,uint256 tokenId, address sender){
@@ -38,22 +45,30 @@ contract BlockBid is ReentrancyGuard{
     }
 
     modifier ForSale721(address nftAddress , uint tokenId){
-        listing721 memory displayed = nftListed721[nftAddress][tokenId];
+        listing721 memory displayed = nftListed721[tokenId];
         if(displayed.price <= 0){
             revert NotForSale(nftAddress , tokenId);
         }
         _;
     }
 
-    function getListedNFT721 (address _nftAddress, uint256 _tokenId) public view returns (listing721 memory){
-        return nftListed721[_nftAddress][_tokenId];
+    modifier AuctionExists (uint256 _tokenId){
+        auctionListing721 memory auctionItem = auctionNftListed721[_tokenId];
+        if(auctionItem.tokenId <= 0){
+            revert AuctionItemNotExists(_tokenId);
+        }
+        _;
+    }
+
+    function getListedNFT721 ( uint256 _tokenId) public view returns (listing721 memory){
+        return nftListed721[_tokenId];
     }
 
     function getListedTokens () public view returns (uint256 [] memory){
         return ListedTokens721;
     }
 
-    function sellNft721 (address _nftAddress , uint256 _tokenId , uint256 price, uint256 duration, uint256 buyoutPrice) external Owner721(_nftAddress , _tokenId ,msg.sender){
+    function sellNft721 (address _nftAddress , uint256 _tokenId , uint256 price) external Owner721(_nftAddress , _tokenId ,msg.sender){
         if (price <= 0){
             revert PriceShouldBeGreaterThanZero();
         }
@@ -63,12 +78,12 @@ contract BlockBid is ReentrancyGuard{
             revert DoNotHaveApprovalToSellNft();
         }
   
-        nftListed721[_nftAddress][_tokenId] = listing721(_tokenId, payable(msg.sender) , price, block.timestamp + duration, address(0), 0, false, buyoutPrice);
+        nftListed721[_tokenId] = listing721(_tokenId, payable(msg.sender) , price);
         ListedTokens721.push(_tokenId);
     }
 
     function buyNft721 (address _nftAddress , uint256 _tokenId) external payable ForSale721(_nftAddress , _tokenId) nonReentrant{
-        listing721 memory item721 = nftListed721[_nftAddress][_tokenId];
+        listing721 memory item721 = nftListed721[_tokenId];
         if(msg.value < item721.price){
             revert PriceNotMatched(_nftAddress , _tokenId);
         }
@@ -76,7 +91,7 @@ contract BlockBid is ReentrancyGuard{
         (bool success, ) = item721.owner.call{value: msg.value}("");
         require(success, "Transfer failed");
 
-        delete (nftListed721[_nftAddress][_tokenId]);
+        delete (nftListed721[_tokenId]);
        
         uint i = 0;
         while(ListedTokens721[i] != _tokenId){
@@ -92,31 +107,58 @@ contract BlockBid is ReentrancyGuard{
         IERC721(_nftAddress).safeTransferFrom(item721.owner, msg.sender, _tokenId);
     }
 
-    function bid(address _nftAddress, uint256 _tokenId) external payable {
-        listing721 storage auction = nftListed721[_nftAddress][_tokenId];
-        require(block.timestamp <= auction.auctionEndTime, "Auction already ended.");
-        require(msg.value > auction.highestBid, "There already is a higher bid.");
-        if (msg.value >= auction.buyoutPrice) {
-            // End the auction immediately if the bid is greater than or equal to the buyout price
-            auction.ended = true;
-            auction.owner.transfer(msg.value);
-            IERC721(_nftAddress).safeTransferFrom(auction.owner, msg.sender, _tokenId);
-        } else {
-            if (auction.highestBid != 0) {
-                // Refund the previously highest bidder.
-                payable(auction.highestBidder).transfer(auction.highestBid);
-            }
-            auction.highestBid = msg.value;
-            auction.highestBidder = msg.sender;
+    function auctionNft721(address _nftAddress , uint256 _tokenId ,uint256 minPrice,uint256 auctionduration) external Owner721(_nftAddress , _tokenId , msg.sender){
+
+        uint256 starttime = block.timestamp;
+        uint256 endtime = starttime + auctionduration;
+
+        require (block.timestamp < endtime , "Not sufficient time for auction");
+        require(minPrice >0 , "minPrice should be greater than 0");
+
+            
+        IERC721 nft = IERC721(_nftAddress);
+        if (nft.getApproved(_tokenId) != address(this)){
+            revert DoNotHaveApprovalToSellNft();
         }
+
+        auctionNftListed721[_tokenId] = auctionListing721(_tokenId , payable(msg.sender) , minPrice , endtime, address(0), 0 , false);
+    }
+    
+    
+    // bid function
+    
+    function bid( uint256 _tokenId) external payable AuctionExists(_tokenId){
+        auctionListing721 storage auctionItem = auctionNftListed721[_tokenId];
+
+
+        require(auctionItem.owner != msg.sender  , "Owner cannot bid on their own nft");
+        require(msg.value > auctionItem.highestBid, "There already is a higher bid.");
+
+        if (auctionItem.highestBid > 0) {
+            // Refund the previously highest bidder.
+            (bool success , ) = auctionItem.highestBidder.call{value: auctionItem.highestBid}("");
+            require(success , "fund not returned yet");
+        }
+        auctionItem.highestBid = msg.value;
+        auctionItem.highestBidder = msg.sender;
     }
 
-    function auctionEnd(address _nftAddress, uint256 _tokenId) external {
-        listing721 storage auction = nftListed721[_nftAddress][_tokenId];
+    function auctionEnd(address _nftAddress, uint256 _tokenId) external Owner721(_nftAddress , _tokenId , msg.sender){
+        auctionListing721 memory auction = auctionNftListed721[_tokenId];
+
         require(block.timestamp >= auction.auctionEndTime, "Auction not yet ended.");
         require(!auction.ended, "AuctionEnd has already been called.");
+
         auction.ended = true;
-        auction.owner.transfer(auction.highestBid);
+        (bool success , ) = auction.owner.call{value : auction.highestBid}("");
+        require(success , "Transfer not completed");
+
         IERC721(_nftAddress).safeTransferFrom(auction.owner, auction.highestBidder, _tokenId);
+        delete auctionNftListed721[_tokenId];
+    }
+
+    function getListedAuctionItem721(uint256 _tokenId) public view returns(auctionListing721 memory){
+        return auctionNftListed721[_tokenId];
+
     }
 }
