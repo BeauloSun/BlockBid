353cbff529adb9dd458f121d82d75bcef758108f
Satwik Goyal <satwikgoyal@Satwiks-MacBook-Air.local>
2023-12-19 00:20:59 +0000

    Updated auction and user fund added the auction now works

--- contracts/auction_blockbid.sol
+++ contracts/auction_blockbid.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 error NotForSale(address nftAddress , uint256 tokenId);
 error PriceNotMatched(address _nftAddress , uint256 _tokenId);
@@ -35,6 +36,8 @@ contract BlockBid is ReentrancyGuard{
 
     mapping(uint256 => listing721) nftListed721;
     mapping (uint256 => auctionListing721) auctionNftListed721;
+    mapping(uint256 => mapping(address => uint256)) auctionBids;
+    mapping (uint256 => address[]) auctionBidders;
     mapping (address => uint256) UserFunds;
     uint256[] ListedTokens721;
 
@@ -128,22 +131,26 @@ contract BlockBid is ReentrancyGuard{
 
         auctionNftListed721[_tokenId] = auctionListing721(_tokenId , payable(msg.sender) , minPrice , endtime, address(0), 0 , false);
     }
-    // bid function
     
     function bid( uint256 _tokenId) external payable AuctionExists(_tokenId){
+
+        // check owner should not bid
         auctionListing721 storage auctionItem = auctionNftListed721[_tokenId];
-
-
         require(auctionItem.owner != msg.sender  , "Owner cannot bid on their own nft");
-        require(msg.value > auctionItem.highestBid, "There already is a higher bid.");
 
-        if (auctionItem.highestBid > 0) {
-            // Refund the previously highest bidder.
-            UserFunds[auctionItem.highestBidder] += auctionItem.highestBid;
-            // (bool success , ) = auctionItem.highestBidder.call{value: auctionItem.highestBid}("");
-            // require(success , "fund not returned yet");
+        // check the new added value is greater than previous value + new value
+        uint256 newTotalBid = auctionBids[_tokenId][msg.sender] + msg.value;
+        require(newTotalBid > auctionItem.highestBid, "There already is a higher bid.");
+
+        // update the bids
+        auctionBids[_tokenId][msg.sender] = newTotalBid;
+
+        // update the bidders array if the new bidder does nto exits
+        if(!checkBidderExists(_tokenId, msg.sender)){
+            auctionBidders[_tokenId].push(msg.sender);
         }
-        auctionItem.highestBid = msg.value;
+
+        auctionItem.highestBid = newTotalBid;
         auctionItem.highestBidder = msg.sender;
     }
 
@@ -154,13 +161,37 @@ contract BlockBid is ReentrancyGuard{
         require(!auction.ended, "AuctionEnd has already been called.");
 
         auction.ended = true;
-        // store the user funds
-        UserFunds[auction.owner] += auction.highestBid;
-        // (bool success , ) = auction.owner.call{value : auction.highestBid}("");
-        // require(success , "Transfer not completed");
 
+        // transfer all the money to the seller's fund
+        if(auctionBids[_tokenId][auction.highestBidder] == auction.highestBid){
+            UserFunds[msg.sender] += auctionBids[_tokenId][auction.highestBidder];
+            auctionBids[_tokenId][auction.highestBidder] = 0;
+        }
+
+        // transfer the money to all the bidders fund
+        address[] memory bidders = auctionBidders[_tokenId];
+        for(uint256 i = 0 ; i < bidders.length; i++){
+            UserFunds[bidders[i]] += auctionBids[_tokenId][bidders[i]];
+            delete auctionBids[_tokenId][bidders[i]];
+        }
+
+        
+        delete auctionBidders[_tokenId];
         IERC721(_nftAddress).safeTransferFrom(auction.owner, auction.highestBidder, _tokenId);
         delete auctionNftListed721[_tokenId];
+    }
+
+    function getTheBidDifference(uint256 _tokenId, address currUser) external view returns (uint256){
+         auctionListing721 storage auctionItem = auctionNftListed721[_tokenId];
+
+         if(!checkBidderExists(_tokenId, currUser)){
+            return auctionItem.highestBid;
+         }
+         return auctionItem.highestBid - auctionBids[_tokenId][currUser];
+    }
+
+    function getBiddersLastBid(uint256 _tokenId, address bidder) public view returns(uint256){
+        return auctionBids[_tokenId][bidder];
     }
 
     function getListedAuctionItem721(uint256 _tokenId) public view returns(auctionListing721 memory){
@@ -168,11 +199,13 @@ contract BlockBid is ReentrancyGuard{
 
     }
 
-
     function getUserFunds(address user) public view returns(uint256){
         return UserFunds[user];
     }
 
+    function getBiddersForNft721(uint256 _tokenId) public view returns (address[] memory ){
+        return auctionBidders[_tokenId];
+    }
 
     function getFundBack(address user) external {
         uint256 userfunds = UserFunds[user];
@@ -183,6 +216,16 @@ contract BlockBid is ReentrancyGuard{
         (bool success , ) = payable(user).call{value:userfunds}("");
         require(success , "There was an error in transferring funds");
     }
+
+    function checkBidderExists(uint256 _tokenId, address bidder) public view returns (bool) {
+        address[] storage bidders = auctionBidders[_tokenId];
+        for (uint i = 0; i < bidders.length; i++) {
+            if (bidders[i] == bidder) {
+                return true;
+            }
+        }
+        return false;
+}
 
 
 
