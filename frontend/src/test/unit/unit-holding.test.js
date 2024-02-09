5a44b7dbad9dee3c8a5efdc4e455c74f73cf6ea7
Jiaming Sun <mr_internet1969@outlook.com>
2024-02-09 13:20:23 +0000

    modified axios

--- frontend/src/test/unit/unit-holding.test.js
+++ frontend/src/test/unit/unit-holding.test.js
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Holding } from "../../components/holdings";
 
-test("Holding", () => {
+test("Profile button", () => {
   render(
     <Router>
       <Holding />
@@ -12,6 +12,30 @@ test("Holding", () => {
   );
   const testElement = screen.getByRole("button", {
     name: "Profile & Security",
+  });
+  expect(testElement).toBeInTheDocument();
+});
+
+test("Holding button", () => {
+  render(
+    <Router>
+      <Holding />
+    </Router>
+  );
+  const testElement = screen.getByRole("button", {
+    name: "Holdings",
+  });
+  expect(testElement).toBeInTheDocument();
+});
+
+test("Wallet Button", () => {
+  render(
+    <Router>
+      <Holding />
+    </Router>
+  );
+  const testElement = screen.getByRole("button", {
+    name: "Wallet",
   });
   expect(testElement).toBeInTheDocument();
 });
