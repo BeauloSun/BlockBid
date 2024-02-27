23db9cb369564e94de0a8125b6e17efef2c9dac8
Jiaming Sun <mr_internet1969@outlook.com>
2024-02-27 18:17:02 +0000

    fixed merge conflict on 1155 market routes

--- db/routes/nfts1155marketRoutes.js
+++ db/routes/nfts1155marketRoutes.js
@@ -297,49 +297,6 @@ router.get("/getNftsOnAuction", (req, res) => {
     });
 });
 
-router.get("/getNftsOnAuction", (req, res) => {
-  Nft1155marketplaceModel.aggregate([
-    {
-      $sort: {
-        token_id: 1,
-        listing_id: 1,
-      },
-    },
-    {
-      $group: {
-        _id: "$token_id",
-        doc: {
-          $first: "$$ROOT",
-        },
-        total_available_quantity: {
-          $sum: "$available_quantity",
-        },
-      },
-    },
-    {
-      $addFields: {
-        "doc.available_quantity": "$total_available_quantity",
-      },
-    },
-    {
-      $replaceRoot: {
-        newRoot: "$doc",
-      },
-    },
-    {
-      $match: {
-        on_auction: true,
-      },
-    },
-  ])
-    .then(function (nfts) {
-      res.json(nfts);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
-});
-
 router.post("/getBids", async (req, res) => {
   const { tokenId, listingId } = req.body;
   try {
